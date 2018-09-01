9fdf11d96830b7f7e65dacc31807b474152230e2
keydone <k754708625@gmail.com>
2018-09-01 11:34:40 +0800

    Mod_修复IE10多层弹窗不会自动移除

--- dist/vendor.0ed.js
+++ dist/vendor.0ed.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + "2d4" + ".js";
+/******/ 		script.src = __webpack_require__.p + "" + ({}[chunkId]||chunkId) + "." + "0ed" + ".js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
